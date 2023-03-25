d9d405e1b19bada529a00d360bd03aac290ff869
Mohamed A. Gamal <m.metallized@gmail.com>
2023-03-25 04:02:30 +0200

    :wrench: Terraform: Added force delete for ACR

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "ecr" {
   name                 = "ecr_repo"
   image_tag_mutability = "MUTABLE"
+  force_delete = true
 
   image_scanning_configuration {
     scan_on_push = true
