fdabadd6e1046b1823bd53f458dc88074afccf2b
Mohamed A. Gamal <m.metallized@gmail.com>
2023-04-06 06:26:35 +0200

    :hammer: Script: When config is empty it will not fail and will overwrite with the entry

--- scripts/create_include_config_ssh.sh
+++ scripts/create_include_config_ssh.sh
@@ -2,11 +2,16 @@
 
 #Set ENV_VAR to the project directory
 export CAPSTONE_PROJECT="$(cd .. && export TEST=$PWD && echo $TEST)"
+CONFIG_FILE="$HOME/.ssh/config"
 
-# Create an include entry in ~/.ssh/config
-# and Skip if already available 
-if grep -qE "Include|creds" ~/.ssh/config; then
-  echo "Entry already available in config file"
+if [[ -s "$CONFIG_FILE" ]]; then # If config File is empty
+  # Create an include entry in ~/.ssh/config
+  # and Skip if already available
+  if grep -qE "Include|creds" $CONFIG_FILE; then
+    echo "Entry already available in config file"
+  else
+  sed -i "1s|^|Include $CAPSTONE_PROJECT/creds/config \n |" $CONFIG_FILE
+  fi
 else
-sed -i "1s|^|Include $CAPSTONE_PROJECT/creds/config \n |" ~/.ssh/config
+echo "Include $CAPSTONE_PROJECT/creds/config" > $CONFIG_FILE # overwrite the file directly
 fi
