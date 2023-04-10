fc253ce1ac70db023ea4c816bba6851ecc15a4dd
Mohamed A. Gamal <m.metallized@gmail.com>
2023-04-10 11:56:07 +0200

    :art: Scripts: ssh_config- Switched to function-based
    In preparation for using case statement

---
+++ scripts/config_ssh_modification.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+hello_world(){
+  echo "Hello World!!"
+}
+
+create_include_config_ssh(){
+  #Set ENV_VAR to the project directory
+  CAPSTONE_PROJECT=$(dirname $(find $HOME -type f -name "test.sh"  2> /dev/null | grep -w scripts))
+  CONFIG_FILE="$HOME/.ssh/config"
+  echo $CAPSTONE_PROJECT
+
+  if [[ -s "$CONFIG_FILE" ]]; then # If config File is empty
+    # Create an include entry in ~/.ssh/config
+    # and Skip if already available
+    if grep -qE "Include|creds" $CONFIG_FILE; then
+      echo "Entry already available in config file"
+    else
+    sed -i "1s|^|Include $CAPSTONE_PROJECT/creds/config \n |" $CONFIG_FILE
+    fi
+  else
+  # overwrite the file directly
+  echo "Include $CAPSTONE_PROJECT/creds/config" > $CONFIG_FILE 
+  fi
+}
+
+
+remove_include_config_ssh(){
+  sed -i "/creds/d" ~/.ssh/config
+}
--- scripts/delete_include_config_ssh.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-sed -i "/creds/d" ~/.ssh/config
