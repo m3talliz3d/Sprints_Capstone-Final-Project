fa46a3458212f6fb2fad9a4256a8cc96f042fb6e
Mohamed A. Gamal <m.metallized@gmail.com>
2023-04-11 08:01:12 +0200

    :art: Script: RunMe.sh: Modified case enteries (functions)

--- RunMe.sh
+++ RunMe.sh
@@ -7,36 +7,43 @@ source scripts/infra_deployment.sh
 export CAPSTONE_PROJECT=$PWD
 echo $CAPSTONE_PROJECT
 hello_world
-#create_include_config_ssh
 #sleep 5
-#remove_include_config_ssh
-#terraform_deploy
-#terraform_destroy
-#create_config_file 2>/dev/null && chmod 600 creds/config
 
 
 EXIT=0
 while [ $EXIT -ne 1 ]
 do
-  echo "Choose one of the options to run the capstone project:"
+  echo "Choose one of the options to run the capstone project: (q to quit)"
 
-  echo "1 - Run Prepare Credentials directory"
+  echo "1 - Prepare Credentials directory"
   echo "2 - Run Terrafrom then Run Playbook Ansible"
   echo "3 - Run Terrafrom Only"
   echo "4 - Run Ansible Playbook Only"
   echo "5 - Detroy Terraform (no-confirmation)"
   echo "6 - Cleanup"
-  echo "0 - Exit"
+  echo "q - Exit"
 
   read num
   case $num in
-  0) EXIT=1 ;;
-  1)hello_world;;
+
+  q) EXIT=1
+    echo "Exiting..."
+    sleep 3;;
+  1)
+  mkdir -p creds
+  create_config_file 2>/dev/null && chmod 600 creds/config
+  echo "test";;
   2) ;;
-  3) ;;
+  3) terraform_deploy
+  create_pem_file
+  clear
+  echo "##################################"
+  echo "Terrafrom Deployment is successful"
+  echo "##################################"
+  break;;
   4) ;;
-  5) ;;
+  5) terraform_destroy;;
   6) ;;
-  *)echo "You have chosen unidentified number"
+  *)echo "You have chosen unidentified number, try again..."
   esac
 done
