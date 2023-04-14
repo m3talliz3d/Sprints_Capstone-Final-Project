2657e3df22bed63815ac215ace49e648619d5a02
Mohamed A. Gamal <m.metallized@gmail.com>
2023-04-14 04:34:57 +0200

    :hammer: Script: Templates.sh - Added aws_cred template

--- scripts/templates.sh
+++ scripts/templates.sh
@@ -8,3 +8,15 @@ create_config_file(){
     IdentityFile $CAPSTONE_PROJECT/creds/ansible-keypair.pem
     StrictHostKeyChecking no`
 }
+
+create_aws_creds(){
+  echo "Type your AWS_ID:"
+  read AWS_ID
+  echo "Type your AWS_ID:"
+  read AWS_KEY
+  `cat << EOT > creds/aws_creds
+{
+  "AWS_ACCESS_KEY_ID" : "$AWS_ID",
+  "AWS_SECRET_ACCESS_KEY" : "$AWS_KEY"
+}`
+}
