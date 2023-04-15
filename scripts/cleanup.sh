95a0b6d361bfff57a6204e5da14f99dc8721da76
Mohamed A. Gamal <m.metallized@gmail.com>
2023-04-15 19:07:47 +0200

    :hammer: Script: cleanup.sh - Now you cleanup will remove creds directory.

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -3,3 +3,7 @@
 remove_include_config_ssh(){
   sed -i "/creds/d" ~/.ssh/config
 }
+
+delete_creds_dir(){
+  rm -rf creds/
+}
--- scripts/echo_scripts.sh
+++ scripts/echo_scripts.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
 echo_terraform_deploy(){
-  echo "##################################"
+  echo -e "\n##################################"
   echo "Terrafrom Deployment is successful"
   echo "Created creds/ansible-keypair.pem "
   echo "##################################"
 }
 
 echo_terraform_destroy(){
-  echo "##################################"
+  echo -e "\n##################################"
   echo "Terrafrom Deployment is Destroyed"
   echo "##################################"
 }
 
 echo_ansible_ansible(){
-  echo "##################################"
+  echo -e "\n##################################"
   echo "Ansible Playbook is successful"
+  echo "##################################"
+}
+
+echo_remove_include_config_ssh(){
+  echo -e "\n###################################"
+  echo "Removed custom line on /.ssh/config"
+  echo "###################################"
+}
+
+echo_delete_creds_dir(){
+  echo -e "\n##################################"
+  echo "    Removed 'creds' Directory     "
   echo "##################################"
 }
